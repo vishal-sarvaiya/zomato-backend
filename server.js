5ec64f2b70e80c43093e10437e5c3ae63e5e240b
Vishal Sarvaiya <vishal.sarvaiya@technostacks.com>
2023-10-05 16:41:27 +0530

    server.js modified

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require("express")
 const cors = require("cors")
-const mongoose =require("mongoose")
+const mongoose = require("mongoose")
 const app = express()
 const path = require("path")
 app.use(express())
-// app.use(cors())
-app.use(express.json())
 
-app.use(cors({
-    origin: ["https://zomato-vishal-sarvaiya.vercel.app/","https://zomato-vishal-sarvaiya.vercel.app"],
-    methods: ["POST", "GET", "PUT", "DELETE", "PATCH"],
-    credentials : true
-}))
+const corsOptions = {
+    origin: 'https://zomato-vishal-sarvaiya.vercel.app', // Replace with your Vercel app's URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Include cookies in cross-origin requests if needed
+};
+
+app.use(cors(corsOptions));
+app.use(express.json())
 
 // {
 //     "origin": "*",
@@ -37,25 +38,25 @@ app.use(orderRoute)
 const cartRoute = require("./app/routes/Cart/cartRoute")
 app.use(cartRoute)
 
-const cityRoute  = require("./app/routes/City/cityRoute")
+const cityRoute = require("./app/routes/City/cityRoute")
 app.use(cityRoute)
 
 const questionRoute = require("./app/routes/Question/questionRoute")
 app.use(questionRoute)
 
-app.use('/app/uploads',express.static('app/uploads'))
+app.use('/app/uploads', express.static('app/uploads'))
 
 mongoose
-// .connect("mongodb://localhost:27017/Zomato")
-.connect(process.env.MONGODB_URL)
-.then(() =>{
-    console.log("connected to mongodb");
-})
-.catch((error)=>{
-    console.log("mongodb connection Error:",error);
-})
+    // .connect("mongodb://localhost:27017/Zomato")
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch((error) => {
+        console.log("mongodb connection Error:", error);
+    })
 
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("App is Listening on Port 8000");
 })
 
