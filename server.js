5775f80264d997e3db71eb1d235106ebe3d9be45
Vishal Sarvaiya <vishal.sarvaiya@technostacks.com>
2023-10-05 17:40:17 +0530

    server.js modified

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ app.use(express())
 
 const corsOptions = {
     origin: 'https://zomato-vishal-sarvaiya.vercel.app', // Replace with your Vercel app's URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, // Include cookies in cross-origin requests if needed
+    // methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // credentials: true, 
+    // Include cookies in cross-origin requests if needed
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors({
+origin: 'https://zomato-vishal-sarvaiya.vercel.app'
+}))
 app.use(express.json())
 
 
